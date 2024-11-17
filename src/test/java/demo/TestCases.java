794907504a22fff0488e48ba6d188cb1beaa915d
Prachi <prachimore811@gmail.com>
2024-11-17 12:23:43 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,21 +1,26 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+import java.util.List;
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
     ChromeDriver driver;
+    Wrappers wrappers=new Wrappers();
 
     /*
      * TODO: Write your tests here with testng @Test annotation. 
@@ -47,6 +52,53 @@ public class TestCases {
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+
+    }
+    @Test
+    public void testCase01() throws InterruptedException {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2000));
+//     finding out web element for name
+        driver.get("https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+        WebElement name = driver.findElement(By.xpath("//div[@class='rFrNMe k3kHxc RdH0ib yqQS1 zKHdkd']//input"));
+        wrappers.sendKeys(name,"Crio Learner");
+//    finding out web element for "Why are you practicing Automation?
+
+        WebElement practicing_Automation = driver.findElement(By.tagName("textarea"));
+        long epoch = System.currentTimeMillis();
+        wrappers.sendKeys(practicing_Automation,"I want to be the best QA Engineer! "+epoch);
+        WebElement exprience = driver.findElement(By.xpath("//div[@data-value='3 - 5']/div/div"));
+        wrappers.elementClick(exprience);
+// Selecting a skill set using selectskill method. it's user defined
+        wrappers.selectSkill(driver,"Java");
+        wrappers.selectSkill(driver,"Selenium");
+        wrappers.selectSkill(driver,"TestNG");
+//Selecting addressed learner
+        WebElement learnerAddressed=driver.findElement(By.xpath("//span[text()='Choose']//parent::div"));
+        wrappers.elementClick(learnerAddressed);
+        wrappers.selectAddressedBy(driver,"Ms");
+        Thread.sleep(2000);
+// Putting a date before 7 days ago.
+        WebElement dateSelected=driver.findElement(By.xpath("//input[@type='date']"));
+       wrappers.sendKeys(dateSelected,wrappers.date7DaysAgo());
+// Using list of string store the HH and mm and putting into respective field
+       List<String> time=wrappers.setTime();
+       WebElement hour=driver.findElement(By.xpath("//input[@aria-label='Hour']"));
+       wrappers.sendKeys(hour,time.get(0));
+        WebElement minute=driver.findElement(By.xpath("//input[@aria-label='Minute']"));
+        wrappers.sendKeys(minute,time.get(1));
+// Finding out submit button and clicking on that button
+        WebElement submitButton=driver.findElement(By.xpath("//div[@aria-label='Submit']"));
+        submitButton.click();
+// Printing the success message
+        Thread.sleep(3000);
+        WebElement successMessage1=driver.findElement(By.xpath("//div[text()='Thanks for your response, Automation Wizard!']"));
+        WebElement successMessage2=driver.findElement(By.xpath("//a[text()='Submit another response']"));
+
+        System.out.println("First success message is "+successMessage1.getText());
+        System.out.println("Second success message is "+successMessage2.getText());
+
+
+
     }
 
     @AfterTest
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,19 +1,85 @@
 package demo.wrappers;
 
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.zip.DataFormatException;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    By name= By.xpath("");
+
+
+
+
+    public void sendKeys(WebElement element,String message)  {
+        element.sendKeys(message);
+    }
+    public void elementClick(WebElement element)  {
+        element.click();
+    }
+
+    public void selectSkill(WebDriver driver,String skill)  {
+        List<WebElement> choices=driver.findElements(By.xpath("//div[@jscontroller='sW52Ae']//div[@role='listitem']//div[@role='checkbox']"));
+
+        for(WebElement e:choices)  {
+            String choice =e.getAttribute("aria-label");
+            if(choice.equals(skill))  {
+                e.click();
+            }
+        }
+    }
+
+    public void selectAddressedBy(WebDriver driver,String title)  {
+        List<WebElement> choices=driver.findElements(By.xpath("//div[@class='OA0qNb ncFHed QXL7Te']//div[@jsname='wQNmvb']"));
+
+        for(WebElement e:choices)  {
+            String choice =e.getAttribute("data-value");
+            if(choice.equals(title))  {
+                e.click();
+            }
+        }
+    }
+
+    public String date7DaysAgo()  {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-mm-yyyy");
+
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE,-7);
+        String dayBefore=sdf.format(cal.getTime());
+
+        return dayBefore;
+    }
+    public List<String> setTime()  {
+        List<String >time=new ArrayList<>();
+//        System.out.println("List is "+time);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+
+        Calendar cal = Calendar.getInstance();
+        String systemTime=sdf.format(cal.getTime());
+
+        String arr[]=systemTime.split(":");
+        time.add(arr[0]);
+        time.add(arr[1]);
+//        System.out.println(time);
+        return time;
+    }
 }
